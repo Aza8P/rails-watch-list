d1f6b5c427070962317a69a91f48af07884ab0e3
Ann Altantulga <a3jargal@icloud.com>
2022-08-01 23:33:05 +0800

    on the way there

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,11 +1,21 @@
 class BookmarksController < ApplicationController
   def new
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
   end
 
   def create
-    @bookmark = Bookrmark.new(params_bookmark)
+    @list = List.find(params[:list_id])
+    puts "params=> #{params}"
+    params[:bookmark][:movie].each do |movie_id|
+      Bookmark.create(movie_id: movie_id, list: @list)
+    end
+    redirect_to list_path(@list)
+  end
 
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id)
   end
 end

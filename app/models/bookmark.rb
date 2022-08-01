3e6cd21b13cf3fadf7811738519d64f363c27334
Ann Altantulga <a3jargal@icloud.com>
2022-08-01 13:57:16 +0800

    set associations & validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,8 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :comment, minimum: 6, allow_blank: true
+  validates :comment, length: { minimum: 6 }, allow_blank: false
   validates :movie, uniqueness: { scope: :list }
+  validates :movie, presence: true
+  validates :list, presence: true
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,6 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+
+  validates :name, allow_blank: false, presence: true, uniqueness: true
 end

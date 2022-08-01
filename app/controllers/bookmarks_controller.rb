b541bf92e23174b6f7abc278f6bf883d1cf66ed6
Ann Altantulga <a3jargal@icloud.com>
2022-08-01 17:32:24 +0800

    created bookmark controller & create & new views

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,11 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookrmark.new(params_bookmark)
+
+  end
+end
